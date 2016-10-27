0cc34b7defc09b7fd28dd0a470196b5962ab6581
Dario Molina <dariomolina@student.hartnell.edu>
2016-10-27 12:17:52 -0700

    Change the format of the code in each files, added extra comments and documentation.

--- entertainment2.py
+++ entertainment2.py
@@ -1,14 +1,43 @@
-import media#include media file where we have the code for the movie objects
-import fresh_tomatoes#include this file that allows us to display the movies in a webiste
+import media
+import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life","http://cdn.collider.com/wp-content/uploads/toy-story-poster1.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc") #creating an instance of an object toy story with the necessary requirements
+'''
+Author: Dario Molina
+Date: 10/21/16
+Description: Create instances of object Movie, where program is compiled and \
+run it creates the website displaying movie titles.
+'''
+# creating an instance of an object Toy Story with the necessary arguments as \
+# title, movie description, poster url, and trailer url
 
+toy_story = media.Movie(
+	"Toy Story", 
+	"A story of a boy and his toys that come to life",
+	"http://cdn.collider.com/wp-content/uploads/toy-story-poster1.jpg", # noqa
+	"https://www.youtube.com/watch?v=KYz2wyBy3kc") # noqa
 
-avatar = media.Movie("Avatar","A marine on an alien planet","http://www.impawards.com/2009/posters/avatar.jpg","https://www.youtube.com/watch?v=5PSNL1qE6VY")#creating an instance of an object avatar with the necessary requirements
+# creating an instance of an object Avatar with the necessary arguments as \
+# title, movie description, poster url, and trailer url
 
-spider_man = media.Movie("Spider Man", "A boy who gets bitten by a spider and becomes a super hero.","http://cdn.collider.com/wp-content/uploads/amazing-spider-man-movie-poster.jpg","https://www.youtube.com/watch?v=__kA4B_ut0s")#creating an instance of an object spider man with the necessary requirements
+avatar = media.Movie(
+	"Avatar",
+	"A marine on an alien planet",
+	"http://www.impawards.com/2009/posters/avatar.jpg", # noqa
+	"https://www.youtube.com/watch?v=5PSNL1qE6VY") # noqa
 
+# creating an instance of an object Spider Man with the necessary arguments as \
+# title, movie description, poster url, and trailer url
 
-movies = [toy_story, avatar,spider_man]#storing the movies in a list
+spider_man = media.Movie(
+	"Spider Man", 
+	"A boy who gets bitten by a spider and becomes a super hero.",
+	"http://cdn.collider.com/wp-content/uploads/amazing-spider-man-movie-poster.jpg", # noqa
+	"https://www.youtube.com/watch?v=__kA4B_ut0s") # noqa
 
-fresh_tomatoes.open_movies_page(movies)#opening up browser to display website with movies
+# storing the movies in a list
+
+movies = [toy_story, avatar,spider_man]
+
+# opening up browser to display website with movies
+
+fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,20 +1,28 @@
-import webbrowser#include webbroswer that will allow us to display trailer of movie
+import webbrowser
 
 
-class Movie():#name of the classs
-#documentation
+# name of the class
+
+class Movie():
+
+# documentation
+
 	"""Author: Dario Molina
-Date: 10/16
+Date: 10/21/16
 Description: This class provides a way to store movie related information"""
 
-	def __init__(self, movie_title, movie_storyLine, poster_image, trailer_youtube):#constructor of movie object
+# constructor of movie object
+
+	def __init__(self, movie_title, movie_storyLine, poster_image, trailer_youtube):
 		self.title = movie_title
 		self.storyLine = movie_storyLine
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 		self.movieRatings = 10
 
-	def show_trailer(self):#function that will allow us to display the movie trailer
+# function that will allow us to display the movie trailer
+
+	def show_trailer(self):
 		webbrowser.open(self.trailer)
 
 
